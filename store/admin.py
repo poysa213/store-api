d522ca6742dee0b7336dd3fea4c82d60cb2690f6
Youcef Hanaia <y.hanaia@esi-sba.dz>
2022-10-07 16:12:31 +0100

    adding some admin page customization

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,59 @@
 from django.contrib import admin
-from .models import Product
+from django.db.models.aggregates import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from .models import Product, Collection, Customer, Order
 # Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 50 :
+            return 'Low'
+        return 'Ok'
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = ( reverse('admin:store_product_changelist')
+         + '?' 
+         + urlencode({
+            'collection__id': str(collection.id)
+         }) )
+        return  format_html("<a href={}>{}</a>", url , collection.products_count )
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','customer_name']
+    list_select_related = ['customer']
+
+    def customer_name(self, order):
+        return order.customer
+    class Meta:
+        ordering = ['first_name', 'last_name']
--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -11,17 +11,26 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
-
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -40,7 +49,9 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
+   
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
